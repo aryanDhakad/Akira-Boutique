31a2716ad5998d47ad7ac78a0e2d1edb2eea3150
aryanDhakad <67039949+aryanDhakad@users.noreply.github.com>
2021-12-22 17:35:10 +0530

    New Logo and Fading Effect.

--- js/index.js
+++ js/index.js
@@ -1,39 +1,59 @@
-$('.single-item').slick({
+var rellax = new Rellax(".rellax");
 
-    mobileFirst: true,
+$(".single-item").slick({
+  mobileFirst: true,
 
-    autoplay: true,
-    autoplaySpeed: 2000,
-    dots: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    infinite: true,
-    responsive: [{
-            breakpoint: 1024,
-            settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
-            }
-        },
-        {
-            breakpoint: 450,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
-            }
-        },
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 450,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
 
-        // You can unslick at a given breakpoint now by adding:
-        // settings: "unslick"
-        // instead of a settings object
-    ]
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
 
+  // prevArrow: '<div class=" btn shadow-lg rounded-pill prev-arrow">Previous</div>',
+  // nextArrow: '<div class=" btn shadow-lg rounded-pill next-arrow">Next</div>'
+});
 
+const items = document.querySelectorAll(".animate_1");
 
-    // prevArrow: '<div class=" btn shadow-lg rounded-pill prev-arrow">Previous</div>',
-    // nextArrow: '<div class=" btn shadow-lg rounded-pill next-arrow">Next</div>'
+const appearOptions = {
+  threshold: 0.5,
+  rootMargin: "-100px 0px 0px 0px",
+};
+
+const appearOnScroll = new IntersectionObserver(function (entries) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("fadeIn");
+      appearOnScroll.unobserve(entry.target);
+    }
+  });
+}, appearOptions);
+
+items.forEach((item) => {
+  appearOnScroll.observe(item);
 });
