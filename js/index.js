d71ce450da688c7b31b347ed9d87cdcf87383a62
aryanDhakad <67039949+aryanDhakad@users.noreply.github.com>
2021-12-23 14:35:28 +0530

    new background

--- js/index.js
+++ js/index.js
@@ -37,6 +37,43 @@ $(".single-item").slick({
   // prevArrow: '<div class=" btn shadow-lg rounded-pill prev-arrow">Previous</div>',
   // nextArrow: '<div class=" btn shadow-lg rounded-pill next-arrow">Next</div>'
 });
+$(".single-item-1").slick({
+  mobileFirst: true,
+
+  autoplay: true,
+  autoplaySpeed: 2000,
+  dots: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  infinite: true,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 450,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+      },
+    },
+
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+
+  // prevArrow: '<div class=" btn shadow-lg rounded-pill prev-arrow">Previous</div>',
+  // nextArrow: '<div class=" btn shadow-lg rounded-pill next-arrow">Next</div>'
+});
 
 const items = document.querySelectorAll(".animate_1");
 
